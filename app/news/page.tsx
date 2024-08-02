858f87a486c169d2b41dd62adb9d21190711250f
reema <reemaraheem01@gmail.com>
2024-08-02 15:40:47 +0530

    create newsroom footer and one card component

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 
 import Navbar from './components/Navbar'
-import contactbg from "/public/contact.jpeg";
+import CardNews from './components/CardNews'
+import { Footer } from './components/Footer'
 
 
 function page() {
   return (
     <>
     <div className='m-20'>
+    <div className=''>
       <h1 className='text-5xl font-extrabold mb-8 text-gray-800'>Newsroom</h1>
     <Navbar />
+    </div>
+    <div className=''> 
+       <CardNews />
+    </div>
+    </div>
+    <div className=''>
+    <Footer />
     </div>
     </>
   )
